c9ac424d5b2f47d8652adcd9746f92607c5b6cc3
Danilo Velasquez Urrutia <danilo.velasquez@gmail.com>
2018-08-17 16:05:45 -0300

    typescript-listing-item: * Added responsive index.html. * Label is optional.

--- widgets/ts-listing-item/src/elements/listing-item.ts
+++ widgets/ts-listing-item/src/elements/listing-item.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:max-line-length no-trailing-whitespace */
 class ListingItem extends HTMLElement {
     constructor() {
         super();
@@ -104,13 +105,12 @@ class ListingItem extends HTMLElement {
     }
 
     get template(): string {
-        /* tslint:disable:max-line-length */
         return `
       <link rel="stylesheet" type="text/css" href="https://static.yapo.cl/shared/fonts/fa-5.0.13/css/fontawesome-all.css">
       <div id="ad-${this.adId}" class="listingItem-box">
         <div class="listingItem-image __mainColumn">
-          <span class="listingItem-imageLabel">${this.label}</span>
-          <img src="${this.url}" alt="${this.title}"/>
+          ${this.label ? `<span class="listingItem-imageLabel">${this.label}</span>` : ``}
+          <img src="${this.url}" alt="${this.title}"/>          
         </div>
 
         <div class="listingItem-info __mainColumn">
@@ -145,166 +145,178 @@ class ListingItem extends HTMLElement {
     }
 
     get cssStyle(): string {
-        /* tslint:disable:max-line-length */
         return `
-    <style>
-        :host {
-          border-bottom: 2px solid #F8F8F8;
-          border-top: 2px solid #F8F8F8;
-          border-radius: 5px;
-          margin: 0;
-          padding: 0;
-          width: 100%;
-        }
-
-        * {
-          font-family: Helvetica, Arial, sans-serif;
-        }
-
-        .listingItem-box {
-          background-color: var(--listing-item-background-color, #FFFFFF);
-          color: #000000;
-          display: flex;
-          flex-direction: row;
-          flex-wrap: nowrap;
-          justify-content: space-between;
-          height: 140px;
-          width: 100%;
-        }
-
-        .listingItem-box .__mainColumn{
-          display: flex;
-          flex-direction: column;
-        }
-
-        .listingItem-box .__hidden{
-          display: none;
-        }
-
-        .listingItem-image {
-          min-width: 122px;
-          max-width: 122px;
-          position: relative;
-        }
-
-        .listingItem-image img {
-          height: 100%;
-          object-fit: cover;
-          object-position: 50% 50%;
-        }
-
-        .listingItem-imageLabel {
-          background-color: #5f259f;
-          border-radius: 5px;
-          color: #FFFFFF;
-          font-size: 10px;
-          left: 5%;
-          padding: 5px;
-          position: absolute;
-          top: 5%;
-        }
-
-        .listingItem-info {
-          flex-grow: 1;
-          justify-content: space-between;
-          padding: 10px;
-          padding-top: 19px;
-          padding-right: 0;
-          max-width: 85%;
-        }
-
-        .listingItem-infoTitle {
-          font-size: 14px;
-          font-weight: 100;
-          height: 32px;
-          line-height: normal;
-          margin: 0;
-          overflow: hidden;
-        }
-
-        .listingItem-infoPrice {
-          font-size: 18px;
-          font-weight: bold;
-        }
-
-        .listingItem-infoAdParams {
-          display: flex;
-          flex-direction: row;
-          justify-content: flex-start;
-          list-style-type: none;
-          margin: 0;
-          padding: 0;
-        }
-
-        .listingItem-infoBottom {
-          font-size: 10px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-between;
-        }
-
-        .listingItem-infoBottomDate {
-          align-self: flex-end;
-          color: grey;
-          margin-right: 10px;
-        }
-
-        .listingItem-infoBottomType {
-          align-self: flex-end;
-          color: #4376b0;
-          font-size: 9px;
-          text-transform: uppercase;
-          justify-content: center;
-        }
-
-        .listingItem-infoIcons {
-          font-size: 22px;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          padding: 10px 3px;
-          min-width: 20px;
-          max-width: 20px;
-        }
-
-        .listingItem-infoLocation {
-          flex-grow: 1;
-          font-size: 13px;
-          font-weight: 100;
-          display: none;
-          flex-direction: column;
-          padding-left: 30px;
-          justify-content: center;
-          width: 35%;
-        }
-
-        .listingItem-infoLocation .__locationRow {
-          margin-bottom: 10px;
-        }
-
-        .listingItem-infoLocation .__locationRow:last-child {
-          margin-bottom: 0;
-        }
-
-        .listingItem-infoLastBottom {
-          color: #4376b0;
-        }
-
-        @media (min-width: 700px) {
-          .listingItem-info {
-            min-width: 45%;
-            max-width: 45%;
-          }
-
-          .listingItem-infoBottom {
-            justify-content: flex-start;
-          }
-
-          .listingItem-infoLocation {
-            display: flex;
-          }
-        }
-      </style>
+            <style>
+                :host {
+                  border-bottom: 2px solid #F8F8F8;
+                  border-top: 2px solid #F8F8F8;
+                  border-radius: 5px;
+                  margin: 0;
+                  padding: 0;
+                  width: 100%;
+                }
+        
+                * {
+                  font-family: Helvetica, Arial, sans-serif;
+                }
+        
+                .listingItem-box {
+                  background-color: var(--listing-item-background-color, #FFFFFF);
+                  color: #000000;
+                  display: flex;
+                  flex-direction: row;
+                  flex-wrap: nowrap;
+                  justify-content: space-between;
+                  height: 140px;
+                  width: 100%;
+                }
+        
+                .listingItem-box .__mainColumn{
+                  display: flex;
+                  flex-direction: column;
+                }
+        
+                .listingItem-box .__hidden{
+                  display: none;
+                }
+        
+                .listingItem-image {
+                  min-width: 122px;
+                  max-width: 122px;
+                  position: relative;
+                }
+        
+                .listingItem-image img {
+                  height: 100%;
+                  object-fit: cover;
+                  object-position: 50% 50%;
+                }
+        
+                .listingItem-imageLabel {
+                  background-color: #5f259f;
+                  border-radius: 5px;
+                  color: #FFFFFF;
+                  font-size: 10px;
+                  left: 5%;
+                  padding: 5px;
+                  position: absolute;
+                  top: 5%;
+                }
+        
+                .listingItem-info {
+                  flex-grow: 1;
+                  justify-content: space-between;
+                  padding: 10px;
+                  padding-top: 19px;
+                  padding-right: 0;
+                  max-width: 85%;
+                }
+        
+                .listingItem-infoTitle {
+                  font-size: 14px;
+                  font-weight: 100;
+                  height: 32px;
+                  line-height: normal;
+                  margin: 0;
+                  overflow: hidden;
+                }
+        
+                .listingItem-infoPrice {
+                  font-size: 18px;
+                  font-weight: bold;
+                }
+        
+                .listingItem-infoAdParams {
+                  display: flex;
+                  flex-direction: row;
+                  justify-content: flex-start;
+                  list-style-type: none;
+                  margin: 0;
+                  padding: 0;
+                }
+        
+                .listingItem-infoBottom {
+                  font-size: 10px;
+                  display: flex;
+                  flex-direction: row;
+                  justify-content: space-between;
+                }
+        
+                .listingItem-infoBottomDate {
+                  align-self: flex-end;
+                  color: grey;
+                  margin-right: 10px;
+                }
+        
+                .listingItem-infoBottomType {
+                  align-self: flex-end;
+                  color: #4376b0;
+                  font-size: 9px;
+                  text-transform: uppercase;
+                  justify-content: center;
+                }
+        
+                .listingItem-infoIcons {
+                  font-size: 22px;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-between;
+                  padding: 10px 3px;
+                  min-width: 20px;
+                  max-width: 20px;
+                }
+        
+                .listingItem-infoLocation {
+                  flex-grow: 1;
+                  font-size: 13px;
+                  font-weight: 100;
+                  display: none;
+                  flex-direction: column;
+                  padding-left: 30px;
+                  justify-content: center;
+                  width: 35%;
+                }
+        
+                .listingItem-infoLocation .__locationRow {
+                  margin-bottom: 10px;
+                }
+        
+                .listingItem-infoLocation .__locationRow:last-child {
+                  margin-bottom: 0;
+                }
+        
+                .listingItem-infoLastBottom {
+                  color: #4376b0;
+                }
+        
+                @media (min-width: 700px) {
+                  .listingItem-info {
+                    min-width: 45%;
+                    max-width: 45%;
+                  }
+        
+                  .listingItem-infoBottom {
+                    justify-content: flex-start;
+                  }
+        
+                  .listingItem-infoLocation {
+                    display: flex;
+                  }
+                }
+                /* This rule only applies for Safari*/
+                @media not all and (min-resolution:.001dpcm) { 
+                    @supports (-webkit-appearance:none) {
+                    
+                        .listingItem-image img {                     
+                            display:none;                                                
+                        }
+                        .listingItem-image {
+                            background-image: url("${this.url}");
+                            background-size: cover;
+                        }
+                    }
+                }
+              </style>
     `;
     }
 }
