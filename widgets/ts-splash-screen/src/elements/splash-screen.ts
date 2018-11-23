29b44c9644a284587a74d84449eb8a763f63c613
MiguelMora <miguelmora@miguelmora.local>
2018-11-23 12:40:17 -0300

    update styles

--- widgets/ts-splash-screen/src/elements/splash-screen.ts
+++ widgets/ts-splash-screen/src/elements/splash-screen.ts
@@ -19,13 +19,9 @@ class SplashScreen extends LitElement {
 
     return html`
     <style>
+  
     *{box-sizing: content-box }
      .splashScreen {
-        position: absolute;
-        top: 0;
-        right: 0;
-        z-index: 9999999;
-        overflow-y: scroll;
         width: 100%;
         height: 100vh;
         display: flex;
